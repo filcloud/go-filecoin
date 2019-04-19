470b73fa9062d6603837741987a27e0e53abc46c
frrist <forrest@protocol.ai>
2019-04-18 17:35:33 -0700

    add tracing to node and wite up config

--- node/block.go
+++ node/block.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/pkg/errors"
+	"go.opencensus.io/trace"
 
 	"github.com/filecoin-project/go-filecoin/net/pubsub"
 	"github.com/filecoin-project/go-filecoin/types"
@@ -14,6 +15,20 @@ const BlockTopic = "/fil/blocks"
 
 // AddNewBlock receives a newly mined block and stores, validates and propagates it to the network.
 func (node *Node) AddNewBlock(ctx context.Context, b *types.Block) (err error) {
+	ctx, span := trace.StartSpan(ctx, "Node.AddNewBlock")
+	defer func() {
+		if err != nil {
+			span.AddAttributes(trace.StringAttribute("error", err.Error()))
+		}
+		span.End()
+	}()
+	span.AddAttributes(
+		trace.StringAttribute("block", b.Cid().String()),
+		trace.StringAttribute("miner", b.Miner.String()),
+		trace.Int64Attribute("height", int64(b.Height)),
+		trace.Int64Attribute("nonce", int64(b.Nonce)),
+	)
+
 	// Put block in storage wired to an exchange so this node and other
 	// nodes can fetch it.
 	log.Debugf("putting block in bitswap exchange: %s", b.Cid().String())
@@ -38,10 +53,25 @@ func (node *Node) processBlock(ctx context.Context, pubSubMsg pubsub.Message) (e
 		return nil
 	}
 
+	ctx, span := trace.StartSpan(ctx, "Node.processBlock")
+	defer func() {
+		if err != nil {
+			span.AddAttributes(trace.StringAttribute("error", err.Error()))
+		}
+		span.End()
+	}()
+
 	blk, err := types.DecodeBlock(pubSubMsg.GetData())
 	if err != nil {
 		return errors.Wrap(err, "got bad block data")
 	}
+
+	span.AddAttributes(
+		trace.StringAttribute("block", blk.Cid().String()),
+		trace.StringAttribute("miner", blk.Miner.String()),
+		trace.Int64Attribute("height", int64(blk.Height)),
+		trace.Int64Attribute("nonce", int64(blk.Nonce)),
+	)
 
 	log.Infof("Received new block from network cid: %s", blk.Cid().String())
 	log.Debugf("Received new block from network: %s", blk)
